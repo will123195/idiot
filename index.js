cd584026eaae1572ca6cf7c07780bf6121b07822
will123195 <will123195@gmail.com>
2015-09-20 13:09:02 -0400

    better querystrings

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var request = require('request')
 var querystring = require('querystring')
+var merge = require('deep-extend')
 
 // opts.baseUrl
 // opts.ext
@@ -32,12 +33,19 @@ idiot.prototype.delete = function (resource, opts, cb) {
 // opts.body
 idiot.prototype.run = function (method, resource, opts, cb) {
   var self = this
+  opts = opts || {}
+  cb = cb || function () {}
+  var i = resource.indexOf('?')
+  var query = {}
+  if (i > -1) {
+    query = querystring.parse(resource.substring(i + 1))
+    resource = resource.substring(0, i)
+  }
   if (typeof opts === 'function') {
     cb = opts
     opts = {}
   }
-
-  var query = opts.query || {}
+  merge(query, opts.query)
   var url = self.baseUrl + resource
   if (Object.keys(query).length > 0) {
     url += '?' + querystring.stringify(query)
